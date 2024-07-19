54bae376c86f3b159d0069e28c2dd42a5256a191
UNIDY2002 <UNIDY2002@outlook.com>
2024-07-19 23:15:54 +0800

    Make example pass

--- cachelib/allocator/memory/main.cpp
+++ cachelib/allocator/memory/main.cpp
@@ -3,9 +3,11 @@
 using namespace facebook::cachelib;
 
 int main() {
-    void* memoryStart = (void *) 0x10000000;
-    auto allocator = MemoryAllocator({}, memoryStart, 1024 * 1024 * 1024);
-    auto poolId = allocator.addPool("test", 32 * 1024 * 1024, {1024});
-    printf("Allocate: %p\n", allocator.allocate(poolId, 1024));
-    return 0;
+  auto memory = new uint8_t[0x10000000];
+  void* memoryStart = (void*)((((uint64_t) memory) / sizeof(Slab) + 1) * sizeof(Slab));
+  auto allocator = MemoryAllocator({}, memoryStart, 1024 * 1024 * 1024);
+  auto poolId = allocator.addPool("test", 32 * 1024 * 1024, {1024});
+  printf("Allocate: %p\n", allocator.allocate(poolId, 1024));
+  delete[] memory;
+  return 0;
 }
